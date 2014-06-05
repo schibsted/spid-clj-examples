f1ed929743a08eb4b0da350c963b2bd06152f4f5
Magnar Sveen <magnars@gmail.com>
2014-06-05 14:52:45 +0200

    Add doc markers to paylinks

--- paylinks/src/spid_clojure_paylinks_example/core.clj
+++ paylinks/src/spid_clojure_paylinks_example/core.clj
@@ -13,15 +13,13 @@
 (defonce spid-base-url (:spid-base-url config))
 (defonce our-base-url (:our-base-url config))
 
+;;; Create SPiD client
 (defonce client (spid/create-client client-id
                                     client-secret
                                     {:spid-base-url spid-base-url}))
 
 (defonce token (spid/create-server-token client))
-
-(def products {"sw4" {:description "Star Wars IV" :price 9900 :vat 2400}
-               "sw5" {:description "Star Wars V"  :price 9900 :vat 2400}
-               "sw6" {:description "Star Wars VI" :price 9900 :vat 2400}})
+;;;
 
 (def order-status {"-3" "Expired"
                    "-2" "Cancelled"
@@ -32,6 +30,13 @@
                    "3" "Credited"
                    "4" "Authorized"})
 
+;;; The entirety of our product catalog right here
+(def products {"sw4" {:description "Star Wars IV" :price 9900 :vat 2400}
+               "sw5" {:description "Star Wars V"  :price 9900 :vat 2400}
+               "sw6" {:description "Star Wars VI" :price 9900 :vat 2400}})
+;;;
+
+;;; Create data to POST to /paylink
 (defn prepare-paylink-item [[product-id quantity]]
   (when (> (Integer/parseInt quantity) 0)
     (assoc (products product-id)
@@ -43,9 +48,12 @@
    :cancelUri (str our-base-url "/cancel")
    :clientReference (str "Order number " (rand-int 100000))
    :items (json/write-str (keep prepare-paylink-item products))})
+;;;
 
+;;; Create Paylink
 (defn create-paylink [products]
   (:data (spid/POST client token "/paylink" (create-paylink-data products))))
+;;;
 
 (defn get-order [order-id]
   (:data (spid/GET client token (str "/order/" order-id "/status"))))
@@ -62,8 +70,10 @@
 (defn get-index []
   (serve-page (slurp (io/resource "index.html"))))
 
+;;; Create Paylink and redirect to SPiD
 (defn checkout [request]
   (redirect-to (-> request :params create-paylink :shortUrl)))
+;;;
 
 (defn success [order-id]
   (let [order (get-order order-id)]
