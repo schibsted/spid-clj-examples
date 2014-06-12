a180b9209a90e1d01696fb5115612a198086e40f
Christian Johansen <christian@cjohansen.no>
2014-06-12 13:11:43 +0200

    Working Clojure Direct payment guide

--- direct-payment/src/spid_clojure_direct_payment_example/core.clj
+++ direct-payment/src/spid_clojure_direct_payment_example/core.clj
@@ -37,9 +37,9 @@
 (defonce token (spid/create-server-token client))
 
 ;;; The entirety of our product catalog right here
-(defonce products {"sw4" {:description "Star Wars IV" :price 9900 :vat 2400}
-                   "sw5" {:description "Star Wars V"  :price 9900 :vat 2400}
-                   "sw6" {:description "Star Wars VI" :price 9900 :vat 2400}})
+(defonce products {"sw4" {:name "Star Wars IV" :price 9900 :vat 2400}
+                   "sw5" {:name "Star Wars V"  :price 9900 :vat 2400}
+                   "sw6" {:name "Star Wars VI" :price 9900 :vat 2400}})
 ;;;
 
 ;;; Order status codes
@@ -53,6 +53,14 @@
                        "4" "Authorized"})
 ;;;
 
+;;; Payment identifier types
+(defonce payment-identifier-types {"2" "Credit card"
+                                   "4" "SMS"
+                                   "8" "PayEx Invoice"
+                                   "16" "Voucher"
+                                   "32" "Klarna Invoice"})
+;;;
+
 ;;; Create data to POST to /paylink
 (defn prepare-paylink-item [[product-id quantity]]
   (when (> (Integer/parseInt quantity) 0)
@@ -61,7 +69,9 @@
 
 (defn create-paylink-items [products]
   (json/write-str
-   (keep prepare-paylink-item products)))
+   (->> products
+        (keep prepare-paylink-item)
+        (map #(set/rename-keys % {:name :description})))))
 
 (defn create-paylink-data [products]
   {:title "Quality movies"
@@ -72,17 +82,14 @@
 ;;;
 
 ;;; Create data to POST to /user/{userId}/charge
+(defn create-order-items [products]
+  (json/write-str (keep prepare-paylink-item products)))
+
 (defn create-order-data [user-id products sign-secret]
   (spid/sign-params
-   {:requestReference (str "Order for " user-id)
+   {:requestReference (str "Order #" (rand-int 100000))
     :items (create-order-items products)}
    sign-secret))
-
-(defn create-order-items [products]
-  (json/write-str
-   (->> products
-        (keep prepare-paylink-item)
-        (map #(set/rename-keys % {:description :name})))))
 ;;;
 
 (defn create-paylink [products]
@@ -101,7 +108,7 @@
 
 (defn render-template [template data]
   (reduce (fn [tpl [key val]]
-            (str/replace tpl (str "#{" (name key) "}") val))
+            (str/replace tpl (str "#{" (name key) "}") (str (or val ""))))
           template data))
 
 (defn render-template-resource [file data]
@@ -125,6 +132,15 @@
                     "&oauth_token=" (-> request :session :token))
                {:session nil}))
 
+;;; Extracting order data for the receipt view
+(defn get-order-view [order]
+  {:clientReference (:clientReference order)
+   :status (order-status (:status order))
+   :currency (:currency order)
+   :totalPrice (/ (read-string (:totalPrice order)) 100.0)
+   :paymentIdentifier (payment-identifier-types (:identifierType order))})
+;;;
+
 ;;; Attempting the direct payment, with a Paylink fallback
 (defn checkout [request]
   (let [products (:params request)
@@ -133,23 +149,13 @@
         charge-url (str "/user/" user-id "/charge")
         result (spid/POST client token charge-url params)]
     (if (:success? result)
-      (serve-page
-       (str "<h1>Success!</h1>"
-            "<p>You are now the proud owner of some quality movies. This is what SPiD says:</p>"
-            "<pre>" (with-out-str (json/pprint (:data result))) "</pre>"
-            "<p><a href=\"/\">Go again</a></p>"))
+      (render-template-resource "receipt.html" (get-order-view (:data result)))
       (redirect-to (-> products create-paylink :shortUrl)))))
 ;;;
 
 (defn success [order-id]
   (let [order (get-order order-id)]
-    (serve-page
-     (str "<h1>Success!</h1>"
-          "<p>"
-          (:clientReference order)
-          " is "
-          "<strong>" (order-status (:status order)) "</strong> "
-          "</p>"))))
+    (render-template-resource "receipt.html" (get-order-view order))))
 
 (defn cancel [cancel-page]
   (serve-page
