cb8bc41913bf3a99a0b8227cda643a9f251bbe63
Joakim Wånggren <joakim.wanggren@schibsted.com>
2015-09-18 09:24:00 +0200

    Updated stage/pre server address

--- getting-started/src/getting_started/core.clj
+++ getting-started/src/getting_started/core.clj
@@ -3,7 +3,7 @@
   (:require [spid-client-clojure.core :as spid]))
 
 (defn test-run-api [client-id secret]
-  (let [options {:spid-base-url "https://stage.payment.schibsted.no"}
+  (let [options {:spid-base-url "identity-pre.schibsted.com"}
         client (spid/create-client client-id secret options)
         token (spid/create-server-token client)]
     (clojure.pprint/pprint (spid/GET client token "/endpoints"))))
