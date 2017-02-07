90964047fb4f90c7e81ef417a8d02ba9eaf8d734
Alexander Morland <alek.morland@gmail.com>
2017-02-07 09:50:04 +0100

    Removed Direct Payment example

--- direct-payment/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject spid-clojure-direct-payment-example "0.1.0-SNAPSHOT"
-  :description "SPiD Paylinks example"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [spid-client-clojure "1.1.0"]]
-  :profiles {:dev {:plugins [[lein-ring "0.8.7"]]
-                   :resource-paths ["config"]}}
-  :main spid-clojure-direct-payment-example.core)
